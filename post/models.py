9f217f6ddcfc498bcb73d8ea4ea0ce8e2cf3b27a
Aleksei-Nika <Aleksei.niko42@yandex.ru>
2026-08-24 18:56:50 +0700

    feat: add Post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок',
+        help_text='Введите заголовок поста'
+    )
+    
+    content = models.TextField(
+        verbose_name='Содержание',
+        help_text='Введение текста поста'
+    )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True, #Фиксирует время создания поста
+        verbose_name='Дата создания'
+    )
+    
+    update_at = models.DateTimeField(
+        auto_created=True, #Фиксирует время обновления (редактирования) поста
+        verbose_name='Дата обновления'
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        
+        ordering = ['-created_at']
