16277f7a37533907132622cc91bb169a6720a1d5
Aleksei-Nika <Aleksei.niko42@yandex.ru>
2026-09-07 20:08:50 +0700

    feat: add views for block user

--- userProfile/views.py
+++ userProfile/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import UserUpdateForm, ProfileForm
 from .models import Profile
+from django.core.exceptions import PermissionDenied
+from django.utils import timezone
 
 @login_required
 def profile(request):
@@ -40,3 +43,68 @@ def profile_edit(request):
     }
     return render(request, 'userProfile/profile_edit.html',
                   context)
+    
+@login_required
+def user_profile_view(request, user_id):
+    if not (hasattr(request.user, 'profile') and request.user.is_moderator):
+        raise PermissionDenied('Только модераторы имеют доступы')
+    user = get_object_or_404(User, pk=user_id)
+    profile = get_object_or_404(Profile, user=user)
+    
+    context ={
+        'profile' : profile,
+        'user' : user,
+        'page_title' : f'Профиль {user.username}',
+        'is_moderator_view': True
+    }
+    
+    return render(request, 'userProfile/user_profile_view.html', context)
+
+@login_required
+def toggle_block_user(request, user_id):
+    if not (hasattr(request.user, 'profile') and request.user.is_moderator):
+        raise PermissionDenied('Только модераторы имеют доступы')
+    
+    if request.user.id == user_id:
+        messages.error(request, 'Вы не можете заблокировать себя')
+        return redirect('userProfile:profile')
+    
+    user = get_object_or_404(User, pk=user_id)
+    profile = get_object_or_404(Profile, pk=user_id)
+    
+    if request.method == "POST":
+    
+        action = request.POST.get('action')
+        days = request.POST.get('days')
+        
+        if action == 'block':
+            profile.is_blocked = True
+        
+            if days and days.is_digit():
+                days_int = int(days)
+                if days_int > 0:
+                    profile.blocked_until = timezone.now() + timezone.timedelta(days=days_int)
+                else:
+                    profile.blocked_until = None
+            else:
+                profile.blocked_until = timezone.now() + timezone.timedelta(days=7)
+                
+            profile.save()
+            messages.success(request, 'Пользователь заблокирован')
+        
+        elif action == 'unblock':
+            profile.is_blocked = False
+            profile.blocked_until = None
+            profile.save()
+            messages.success(request, 'Пользователь заблокирован')
+            
+        return redirect('userProfile:user_profile_view, user_id=user.id')
+    
+    context = {
+        'block_user' : user,
+        'profile' : profile,
+        'page_title' : f'Блокировка пользователя {user.username}' 
+    }
+    
+    return render(request, 'userProfile/block_user.html', context)
+    
